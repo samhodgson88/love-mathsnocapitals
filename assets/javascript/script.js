9f09588cfeeea8b9c168e2680d402ad15f254f27
samhodgson88 <sam.hodgson.coding@gmail.com>
2021-11-21 20:58:55 +0000

    added number generator

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,11 +4,44 @@
 // let button of buttons is modern version of let i = 0; i < buttons.length; i++)
 
 document.addEventListener("DOMContentLoaded", function(){
- let buttons = document.getElementsByTagName("button");
- for (let button of buttons){
-     button.addEventListener("click", function (){
-         if (this.getAttribute("data-type")=== "submit") {
-             alert("You clicked Submit!");
-         }
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function (){
+            if (this.getAttribute("data-type")=== "submit") {
+                alert("You clicked Submit!");
+               } else {
+                   let gameType = this.getAttribute("data-type");
+                   runGame(gameType);
+               }
+           });
+       }
+   
+       runGame("addition");
+   
    });
-});
+   
+   /**
+    * The main game "loop", called when the script is first loaded
+    * and after the user's answer has been processed
+    */
+   function runGame(gameType) {
+   
+       // Creates two random numbers between 1 and 25
+       let num1 = Math.floor(Math.random() * 25) + 1;
+       let num2 = Math.floor(Math.random() * 25) + 1;
+   
+       if (gameType === "addition") {
+       displayadditionQuestion(num1, num2);
+       } else {
+           alert(`Unknown game type: ${gameType}`);
+           throw `Unknown game type: ${gameType}. Aborting!`;
+       }
+   
+   }
+   
+   function displayadditionQuestion(operand1,operand2) {
+     document.getElementById('operand1').textContent = operand1;
+     document.getElementById('operand2').textContent = operand2;
+     document.getElementById('operator').textContent = "+";
+   }
+   
